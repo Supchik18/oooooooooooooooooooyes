8df31ba5791742832a1e09c1b52dc4a77a200588
Admin <Admin@DESKTOP-6CV9GRC>
2023-11-18 12:40:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Scoropechatanie
+    {
+        public Scoropechatanie()
+        {
+            x = 0;
+            y = 0;
+            Console.WriteLine(text);
+        }
+        private const string text = "Сейчас из решетки доставки лабораторного оборудования появится грузовой куб. В этом испытании грузовой куб станет вашим верным спутником. Заботьтесь о нем, как о друге. Испытания часто вызывают такие побочные эффекты, как мнительность, галлюцинации и одушевление неодушевленных предметов. Экспериментальный центр напоминает вам о том, что грузовой куб не может угрожать перерезать вам горло и даже не умеет говорить.";
+        public static int x = 0;
+        public static int y = 0;
+        public bool Test()
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = ConsoleColor.Green;
+                ConsoleKeyInfo key = Console.ReadKey();
+                while (key.KeyChar != text[i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(text[i]);
+                    Console.SetCursorPosition(x, y);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    key = Console.ReadKey();
+                }
+                x++;
+                if (x == Console.WindowWidth)
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp16/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Numerics;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    internal class Json
+    {
+        public static void Serialize(List<Player> list)
+        {
+            File.WriteAllText("C:\\Users\\Admin\\Desktop\\Json.json", JsonConvert.SerializeObject(list));
+        }
+        public static List<Player> Deserialize()
+        {
+            return JsonConvert.DeserializeObject<List<Player>>(File.ReadAllText("C:\\Users\\Admin\\Desktop\\Json.json"));
+        }
+        public static bool SearchPlayer(List<Player> list, string Name)
+        {
+            foreach (var element in list)
+            {
+                if (element.Name == Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
